e879ebe9248d2da4f39d28635e4bed71ea0e56d5
Aristotle Pagaltzis <pagaltzis@gmx.de>
2020-05-31 02:53:56 +0200

    initial checkin

---
+++ lib/Net/OAuth2Server/OIDC.pm
@@ -0,0 +1,32 @@
+use strict; use warnings;
+
+package Net::OAuth2Server::OIDC;
+
+our $VERSION = '0.001';
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+Net::OAuth2Server::OIDC - An OpenID Connect server on top of Net::OAuth2Server
+
+=head1 DISCLAIMER
+
+B<I cannot promise that the API is fully stable yet.>
+For that reason, no documentation is provided.
+
+=head1 DESCRIPTION
+
+A usable but tiny implementation of OpenID Connect.
+
+This is also a demonstration of the L<Net::OAuth2Server> design.
+
+=head1 SEE ALSO
+
+This is a very distant descendant of the server portion of L<OIDC::Lite>.
+
+=cut
